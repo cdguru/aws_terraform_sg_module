60245cd81869c6009166d91e3821bc48f8e94bd5
cdguru <56772958+cdguru@users.noreply.github.com>
2024-06-10 10:41:32 -0300

    Fix output names

--- output.tf
+++ output.tf
@@ -2,11 +2,11 @@ output "name" {
   description = "The name of the Security Group"
   value       = aws_security_group.this.name
 }
-output "bucket_id" {
+output "sg_id" {
   description = "The ID of the Security Group"
   value       = aws_security_group.this.id
 }
-output "bucket_arn" {
+output "sg_arn" {
   description = "The ARN of the Security Group"
   value       = aws_security_group.this.arn
 }
